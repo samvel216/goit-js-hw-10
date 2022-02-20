ac73dc1d8df5a948c038456a5cf5b1f3c5fa1fc4
samvel216 <75182258+samvel216@users.noreply.github.com>
2022-02-20 17:14:18 +0200

    1 коммит

---
+++ src/index.js
@@ -0,0 +1,94 @@
+import './css/styles.css';
+const DEBOUNCE_DELAY = 300;
+import Notiflix from 'notiflix';
+const debounce = require('lodash.debounce');
+const inputSearchBoxEl = document.querySelector('#search-box');
+const countryListEl = document.querySelector('.country-list');
+const getMassiveLanguages = (countrLanguages, massiveLanguages) => {
+    for(let language in countrLanguages) {
+        massiveLanguages.push(countrLanguages[language]);
+    }
+    return massiveLanguages;
+    }
+const getFetchResponse = (countryName) =>  {
+    return  fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+    .then(response => {
+    if (!response.ok) {
+    throw new Error(response.status);
+    }
+    return response.json();
+    })}
+const getFlagAndName = (country) => {
+    const markup = country.map((countr) => {  
+        return `<li class="country-markup">
+        <div class="div"> 
+        <img src="${countr.flags.svg}" width="50" height="30" alt="Флаг ${countr.name.official}" />
+        <h2 class="country-name-small">${countr.name.official}<h2>
+        </div>`;
+      }).join("");
+      countryListEl.innerHTML = markup;
+}
+const getFullCard = (country) => {
+    let massiveLanguages = [];
+    const markup = country.map((countr) => {  
+  return `<li class="country-markup">
+  <div class="div"> 
+  <img src="${countr.flags.svg}" width="50" height="30" alt="Флаг ${countr.name.official}" />
+  <h2 class="country-name">${countr.name.official}<h2>
+  </div>
+      <p class="country-capital"><b class="country-capital-b">Capital</b>: ${countr.capital}</p>
+      <p class="country-population"><b class="country-population-b">Population</b>: ${countr.population}</p>
+      <p class="country-languages"><b class="country-languages-b">Languages</b>: ${getMassiveLanguages(countr.languages,massiveLanguages)}</p>
+    </li>`;
+})
+.join("");
+countryListEl.innerHTML = markup;
+}
+const getCard = (countryName) => {  
+    getFetchResponse(countryName).then(country => {
+    let countryKeysLength = Object.keys(country).length;
+    if (countryKeysLength > 10 && countryName.length > 0) {
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+    } else if (countryKeysLength > 2 && countryKeysLength < 10) {
+        getFlagAndName(country);
+    } else if (countryKeysLength < 2) {
+        getFullCard(country);
+    }
+    }    
+    ).catch(error => {
+       if (countryName.length > 5 && error) {
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+        const dssdsd = document.querySelectorAll("li");
+        console.log(dssdsd);
+        for (let sdsd of dssdsd) {
+           sdsd.style.opacity = "0";  
+        }
+       }
+    })   
+}
+const throttledd = debounce(getCard, DEBOUNCE_DELAY);
+let i = 0;
+const opacityLi = () => {
+    const cardLiEl = document.querySelector("li");
+    cardLiEl.style.opacity = "0";
+}
+const opacityAllLi = (cardLiAllEl) => {
+    for (let cardLi of cardLiAllEl) {
+        cardLi.style.opacity = "0";  
+     }
+}
+const inputOnGetCard = (event) => {
+    event.preventDefault();
+    i++;
+    let countryName = event.currentTarget.value;
+    if (countryName.length > 1) {
+        throttledd(countryName);
+    } else if (i > 0 && countryName.length < 1) {
+     const cardLiAllEl = document.querySelectorAll("li");
+     if (cardLiAllEl.length < 2) {
+        opacityLi();
+     }
+     opacityAllLi(cardLiAllEl);
+    }
+}
+inputSearchBoxEl.addEventListener('input', inputOnGetCard);
